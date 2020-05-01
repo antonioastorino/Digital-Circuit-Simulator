6adbf810184291e3691bd3093b6d6dac8ba549ce
Antonio <antonio_jazz@yahoo.it>
2020-05-01 22:39:40 +0200

    Improved 4-bit-counter test

--- src/Tests/UpCounter4Bits-Test.cpp
+++ src/Tests/UpCounter4Bits-Test.cpp
@@ -14,25 +14,32 @@ void upCounter4BitsTest() {
 	DCSDisplayNBits disp0("In", 4);
 	DCSDisplayNBits disp1("Count", 4);
 
+	// connect control signals
 	inArray.connect(&count0, {0, 4}, {0, 4}, {"C_in", "LD", "CLK", "", ""});
+	
+	// connect data-in signals
 	inArray.connect(&count0, {5, 8}, {5, 8});
 
-	count0.connect(&outArray,{0,3}, {0,3}, {"C"});
+	// connect data-in to display (for enhanced visualization purposes)
+	inArray.connect(&disp0, {5, 8}, {0, 3});
+
+	// connect counter output to output object
+	count0.connect(&outArray,{0,3}, {0,3});
 	count0.connect(&outArray,4, 4, {"Cout"});
 	
-	inArray.connect(&disp0, {5, 8}, {0, 3}, {"I"});
+	
 	count0.connect(&disp1, {0, 3}, {0, 3});
 	
 	uint16_t hp = count0.getTimeDelay()/2+1;
 
 	DCSEngine::setHalfClockPeriod(hp);
 
-	inArray[0]->makeSignal(transitions{2,1}, 0, true);		// Carry in
+	inArray[0]->makeSignal(transitions{4,1}, 0, true);		// Carry in
 	inArray[1]->makeSignal(transitions{1,1,1}, 0, true);	// Load
 	inArray[2]->makeSquareWave(hp);							// Clock
 
-	inArray[7]->makeSignal(transitions{1,1, 1, 1}, 0, true);
-	inArray[8]->makeSignal(transitions{1,1, 1, 1}, 0, true);	
+	inArray[7]->makeSignal(transitions{1,1, 1}, 0, true);
+	inArray[8]->makeSignal(transitions{1,1, 1}, 0, true);	
 
 	DCSEngine::run(50 * hp * 2, true);
 }
