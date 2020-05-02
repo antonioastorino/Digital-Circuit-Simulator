229ff26d6ff201329b26f755089648a315892c31
Antonio <antonio_jazz@yahoo.it>
2020-05-02 20:13:00 +0200

    Bug fixed - corrected varible checking

--- bin/build-run.sh
+++ bin/build-run.sh
@@ -3,5 +3,5 @@ curr_dir="`pwd`/`dirname $0`"
 cd $curr_dir/../
 make OPT="$2" 1>/dev/null
 if [ "$?" -eq "0" ]; then
-	[ "$1" == "" ] && build/out-0 "$1" || build/out-"$2" "$1"
+	[ "$2" == "" ] && build/out-0 "$1" || build/out-"$2" "$1"
 fi
