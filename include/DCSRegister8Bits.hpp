2bc5f164b03689de70c77e797a1c58ffe4a95442
Antonio Astorino <antonio.astorino.phd@gmail.com>
2020-05-07 22:32:10 +0200

    Improved documentation

--- include/DCSRegister8Bits.hpp
+++ include/DCSRegister8Bits.hpp
@@ -1,17 +1,31 @@
-/*
-
- Input 0: Output Enable
- Input 1: Clock
- Input 2: Clear
- Input 3: Preset
- Input 4: Load
- Input 5: In bit 0
- Input 6: In bit 1
- ...
-
- Out   0: Out bit 0
- Out   1: Out bit 1
- ...
+/**
+ * @class DCSRegister8Bits
+ * Implements an 8-bit register made up of 8 1-bit registers sharing the same control signals
+ * 
+ * @pinout
+ * In 0    - Output Enable
+ * In 1    - Clock
+ * In 2    - Clear
+ * In 3    - Preset
+ * In 4    - Load
+ * In 5    - Data in 0
+ * In 6    - Data in 1
+ * In 7    - Data in 2
+ * In 8    - Data in 3
+ * In 9    - Data in 4
+ * In 10   - Data in 5
+ * In 11   - Data in 6
+ * In 12   - Data in 7
+ *
+ * Out 0   - Data out 0
+ * Out 1   - Data out 1
+ * Out 2   - Data out 2
+ * Out 3   - Data out 3
+ * Out 4   - Data out 4
+ * Out 5   - Data out 5
+ * Out 6   - Data out 6
+ * Out 7   - Data out 7
+ * @end_pinout
  */
 
 #ifndef DCSRegister8Bits_hpp
