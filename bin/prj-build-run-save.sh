89abde57508d7cf1b44ba1f651a45c714bab60eb
Antonio Astorino <antonio.astorino.phd@gmail.com>
2020-05-23 23:36:48 +0200

    Small bug fixed

--- bin/prj-build-run-save.sh
+++ bin/prj-build-run-save.sh
@@ -6,7 +6,7 @@ o="0"
 [ "$1" != "" ] && n="$1"
 [ "$2" != "" ] && o="$2"
 
-filename="../gui/logic-analyzer/assets/prj-"-n$n-O$o".log"
+filename="../gui/logic-analyzer/assets/prj-n$n-O$o.log"
 
 ./prj-build-run.sh "$n" "$o" | tee $filename
 if ! [ -s $filename ]; then
--- bin/test-build-run-compare.sh
+++ bin/test-build-run-compare.sh
@@ -9,7 +9,7 @@ out_file=../debug/test"-n$n-O$o".log
 
 ./test-build-run.sh "$n" "$o" > $out_file
 if [ "$?" -eq 0 ]; then
-	DIFF=`diff $out_file ../gui/logic-analyzer/assets/test"-n$n-O0".log` &&
+	DIFF=`diff $out_file ../gui/logic-analyzer/assets/test-n$n-O0.log` &&
 	echo "Checking..." &&
 	if [ "$DIFF" != "" ]; then
 		echo "Error: test $n not passed"
